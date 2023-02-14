13bb04aa246f3113e344c0f481da9b1c7970b143
Abner Cunha <abnermcunha@gmail.com>
2023-02-14 00:11:47 -0300

    Preparando telas de Venda

---
+++ SalesWebMvc19/Controllers/SalesRecordsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc19.Models;
+using SalesWebMvc19.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc19.Controllers {
+    public class SalesRecordsController : Controller {
+
+        private SalesRecordService _saleRecordService;
+        private readonly SalesWebMvc19Context _context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch()
+        {
+            //var a = await _saleRecordService.FindAll();
+            return View();
+        }
+
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
---
+++ SalesWebMvc19/Services/SalesRecordService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc19.Data;
+using SalesWebMvc19.Models;
+
+
+namespace SalesWebMvc19.Services {
+    public class SalesRecordService {
+
+        private readonly SalesWebMvc19Context _context;
+        public int MyProperty { get; set; }
+
+        public SalesRecordService(SalesWebMvc19Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<SalesRecord> FindById(int? id)
+        {
+            return await _context.SalesRecord.FindAsync(id);
+        }
+
+        public async Task<List<SalesRecord>> FindAll()
+        {
+            return await _context.SalesRecord.Include(obj => obj.Seller).OrderByDescending(x => x.Date).ToListAsync();
+        }
+
+
+    }
+}
